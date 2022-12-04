b770593152bb2b6e00e03563543ac8f0e747bf1d
dtanwar1 <dtanwar1@binghamton.edu>
2022-12-04 18:13:38 -0500

    Commiting code for KMostFrequent words

---
+++ dtanwar1_asatput1/src/mySentenceAnalyzer/driver/Driver.java
@@ -0,0 +1,47 @@
+package mySentenceAnalyzer.driver;
+
+import mySentenceAnalyzer.myAnalyzer.KMostFrequentWords;
+import mySentenceAnalyzer.myAnalyzer.MyArrayList;
+import mySentenceAnalyzer.myAnalyzer.SpellCheckAmerican;
+import mySentenceAnalyzer.myAnalyzer.Visitor;
+
+/**
+ * @author placeholder
+ *
+ */
+public class Driver {
+	public static void main(String[] args) {
+
+		/*
+		 * As the build.xml specifies the arguments as argX, in case the
+		 * argument value is not given java takes the default value specified in
+		 * build.xml. To avoid that, below condition is used
+		 */
+
+		if (args.length != 5 || args[0].equals("${arg0}") || args[1].equals("${arg1}") || args[2].equals("${arg2}")
+			|| args[3].equals("${arg3}") || args[4].equals("${arg4}")) {
+
+				System.err.println("Error: Incorrect number of arguments. Program accepts 5 argumnets.");
+				System.exit(0);
+		}
+
+		try {
+
+			int topK = Integer.parseInt(args[2]);
+
+			MyArrayList myArrayListObj = new MyArrayList();
+
+			Visitor kFrequentWordsVisitor = new KMostFrequentWords(topK);		
+			Visitor spellCheckVisitor = new SpellCheckAmerican();
+
+			myArrayListObj.accept(kFrequentWordsVisitor);
+			myArrayListObj.accept(spellCheckVisitor);
+			
+		} catch (Exception e) {
+			
+		}
+		
+		
+								 												
+	}
+}
