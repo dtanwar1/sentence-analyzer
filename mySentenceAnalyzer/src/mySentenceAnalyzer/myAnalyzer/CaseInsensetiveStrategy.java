d8bbfa86976ce07afdfe976a7149a316235a8fe5
asatput1 <asatput1@binghamton.edu>
2022-12-08 01:14:15 -0500

    added code for case insesetive strategy

--- mySentenceAnalyzer/src/mySentenceAnalyzer/myAnalyzer/CaseInsensetiveStrategy.java
+++ mySentenceAnalyzer/src/mySentenceAnalyzer/myAnalyzer/CaseInsensetiveStrategy.java
@@ -4,11 +4,19 @@ import java.util.HashMap;
 
 public class CaseInsensetiveStrategy implements StrategyI {
 
-   
     @Override
     public void replaceWords(String[] listWords, HashMap<String, String> wordsLookUp) {
-        
-        
+        HashMap<String, String> wordslookup = new HashMap<String, String>();
+        for(String s : wordsLookUp.keySet()){
+            wordslookup.put(s.toLowerCase(), wordsLookUp.get(s).toLowerCase());
+        }
+        if(listWords!=null && listWords.length >0 && wordslookup!=null && wordslookup.size()>0){
+            for (int i=0; i<= listWords.length -1;i++) {
+                String word = listWords[i].toLowerCase();
+                if(wordslookup.containsKey(word)){
+                    listWords[i] = wordslookup.get(word);
+                }
+            }
+        }    
     }
-    
 }
